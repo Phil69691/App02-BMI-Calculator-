be679dd730b442455525a4f5b50129c1ad97638f
Phil69691 <114498774+Phil69691@users.noreply.github.com>
2023-08-23 01:24:27 +0100

    Update BMI.cs

--- BMI.cs
+++ BMI.cs
@@ -6,10 +6,9 @@ namespace BMI_Calculator
     {
         static void Main(string[] args)
         {
-           
             Console.WriteLine(" ---------- Welcome to the BMI Calculator!----------");
             Console.WriteLine("This app calculates your Body Mass Index (BMI) and provides information on your weight status.");
-            Console.WriteLine("App made by Filip Darmon")
+            Console.WriteLine("App made by Filip Darmon"); // Added a missing semicolon here
 
             Console.Write("Please enter your weight in kilograms: ");
             double weight = Convert.ToDouble(Console.ReadLine());
@@ -17,10 +16,8 @@ namespace BMI_Calculator
             Console.Write("Please enter your height in meters: ");
             double height = Convert.ToDouble(Console.ReadLine());
 
-         
             double bmi = weight / (height * height);
 
-          
             Console.WriteLine("\nYour BMI is: {0:F2}", bmi);
 
             if (bmi < 18.5)
